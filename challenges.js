b8082bbcf14a7717ee9267e15fba976571577ded
mahith chigurupati <mahithchigurupati@gmail.com>
2020-04-19 16:59:07 +0530

    modifications added

--- challenges.js
+++ challenges.js
@@ -8,6 +8,7 @@ var lastDice;
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
     if(gamePlaying) {
+        
         // 1. Random number
         var dice1 = Math.floor(Math.random() * 6) + 1;
         var dice2 = Math.floor(Math.random() * 6) + 1;
